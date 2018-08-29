75051eb096a7de84e5a6a9451d6fbdf355fd8350
Túlio de Oliveira <tulio.ao@gmail.com>
2018-08-29 01:24:27 -0300

    add quotes log to InfoBoard

--- src/actions/CompanyActions.js
+++ src/actions/CompanyActions.js
@@ -1,26 +1,21 @@
 import axios from 'axios';
 import { LOAD_COMPANY, RESET_COMPANY } from './types';
 
-export const loadCompany = symbol => (dispatch) => {
-  axios.get(`https://api.iextrading.com/1.0/stock/${symbol}/company`)
-    .then((response) => {
-      const { data } = response;
-
-      if (data === 'Unknown symbol') {
-        dispatch({
-          type: LOAD_COMPANY,
-          payload: { error: 'Company symbol not found!' }
-        });
-      }
-      else {
-        dispatch({
-          type: LOAD_COMPANY,
-          payload: data
-        });
-      }
-    }).catch((error) => {
-      console.log(error);
-    });
-};
-
 export const resetCompany = () => ({ type: RESET_COMPANY });
+
+export const loadCompany = symbol => (dispatch) => {
+  if (typeof symbol === 'string' && symbol.length > 0) {
+    axios.get(`https://api.iextrading.com/1.0/stock/${symbol}/company`)
+      .then((response) => {
+        dispatch({
+          type: LOAD_COMPANY,
+          payload: response.data
+        });
+      }).catch((error) => {
+        console.log(error);
+      });
+  }
+  else {
+    dispatch(resetCompany());
+  }
+};
--- src/containers/InfoBoard.js
+++ src/containers/InfoBoard.js
@@ -2,18 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-const InfoBoard = ({ company }) => (
-  <button type="button" onClick={() => console.log(company)}>
-    Click to log company
+const log = (...args) => {
+  args.forEach((item) => {
+    console.log(item);
+  });
+};
+
+const InfoBoard = ({ company, quote }) => (
+  <button type="button" onClick={() => log(company, quote)}>
+    Click to log company & quote
   </button>
 );
 
 InfoBoard.propTypes = {
-  company: PropTypes.object.isRequired
+  company: PropTypes.object.isRequired,
+  quote: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-  company: state.company
+  company: state.company,
+  quote: state.quote
 });
 
 export default connect(mapStateToProps)(InfoBoard);
