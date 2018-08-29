31ec50b4ec21d159e2974a5ed12044a11f78845c
Túlio de Oliveira <tulio.ao@gmail.com>
2018-08-29 13:28:57 -0300

    add new components to batch export

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,23 @@
 import Grid from './Grid';
 import Row from './Row';
 import Column from './Column';
+import Link from './Link';
+import Paragraph from './Paragraph';
+import Tag from './Tag';
 import TagList from './TagList';
+import Text from './Text';
 import TextInput from './TextInput';
+import Title from './Title';
 
 export {
   Grid,
   Row,
   Column,
+  Link,
+  Paragraph,
+  Tag,
   TagList,
-  TextInput
+  Text,
+  TextInput,
+  Title
 };
