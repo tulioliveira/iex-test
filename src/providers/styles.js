5d99518c13b61d0612a9928236ce9841d79cae64
Túlio de Oliveira <tulio.ao@gmail.com>
2018-08-29 01:21:41 -0300

    add 'red' and 'green' colors to styles provider

--- src/providers/styles.js
+++ src/providers/styles.js
@@ -2,7 +2,9 @@ const colors = {
   primary: '#343434', // Jet
   secondary: '#2F3061', // Deep Koamaru
   white: '#f7fbff', // Mint Cream
-  gray: '#e4e5e6' // Shadow Blue
+  gray: '#e4e5e6', // Shadow Blue
+  red: '#C14953', // Bittersweet Shimmer
+  green: '#82D173' // Pastel Green
 };
 
 const sizes = {
