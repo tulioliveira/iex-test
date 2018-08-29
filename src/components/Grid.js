20d4c6f096e8ef022cd864d4482096e1ddbd529f
Túlio de Oliveira <tulio.ao@gmail.com>
2018-08-29 09:34:20 -0300

    add Grid, Row and Column components

---
+++ src/components/Grid.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const Grid = styled.div`
+  display: flex;
+  flex-grow: 1;
+`;
+
+export default Grid;
--- src/providers/styles.js
+++ src/providers/styles.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 const colors = {
   primary: '#343434', // Jet
   secondary: '#2F3061', // Deep Koamaru
@@ -11,4 +13,33 @@ const sizes = {
   title: '2.5rem',
 };
 
-export { colors, sizes };
+const alignItemsProperties = [
+  'stretch',
+  'center',
+  'flex-start',
+  'flex-end',
+  'baseline',
+  'initial',
+  'inherit'
+];
+
+const justifyContentProperties = [
+  'flex-start',
+  'flex-end',
+  'center',
+  'space-between',
+  'space-around',
+  'initial',
+  'inherit'
+];
+
+const isAlignProperty = property => (_.includes(alignItemsProperties, property));
+
+const isJustifyProperty = property => (_.includes(justifyContentProperties, property));
+
+export {
+  colors,
+  sizes,
+  isAlignProperty,
+  isJustifyProperty
+};
