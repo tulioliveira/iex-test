dbed4ae42cb1a8f0f841ab268e3c52f7535cfe6e
Túlio de Oliveira <tulio.ao@gmail.com>
2018-08-28 22:48:38 -0300

    create styles provider: colors and sizes

---
+++ src/providers/styles.js
@@ -0,0 +1,12 @@
+const colors = {
+  primary: '#343434', // Jet
+  secondary: '#2F3061', // Deep Koamaru
+  white: '#f7fbff', // Mint Cream
+  gray: '#e4e5e6' // Shadow Blue
+};
+
+const sizes = {
+  title: '2.5rem',
+};
+
+export { colors, sizes };
