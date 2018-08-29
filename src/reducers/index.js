b596f15b40c1ef9f77964610a33f8e07d21441b8
Túlio de Oliveira <tulio.ao@gmail.com>
2018-08-29 19:04:32 -0300

    add Chart reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import CompanyReducer from './CompanyReducer';
 import QuoteReducer from './QuoteReducer';
+import ChartReducer from './ChartReducer';
 
 export default combineReducers({
   company: CompanyReducer,
-  quote: QuoteReducer
+  quote: QuoteReducer,
+  chart: ChartReducer
 });
