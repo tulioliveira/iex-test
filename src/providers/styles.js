27754bca2bb5d0bd79d568bd72af277705dc450d
Túlio de Oliveira <tulio.ao@gmail.com>
2018-08-30 12:36:37 -0300

    add styles provider documentation comments

--- src/providers/styles.js
+++ src/providers/styles.js
@@ -1,15 +1,21 @@
 import _ from 'lodash';
 
+/**
+ * Theme colors
+ */
 const colors = {
   primary: '#343434', // Jet
   secondary: '#8884d8', // Deep Koamaru
   white: '#f7fbff', // Mint Cream
   gray: '#e4e5e6', // Shadow Blue
-  darkGray: '#c3cdd7', // Shadow Blue
+  darkGray: '#c3cdd7', // Shadow Blue - darker
   red: '#C14953', // Bittersweet Shimmer
   green: '#82D173' // Pastel Green
 };
 
+/**
+ * Valid align-items property values
+ */
 const alignItemsProperties = [
   'stretch',
   'center',
@@ -20,6 +26,9 @@ const alignItemsProperties = [
   'inherit'
 ];
 
+/**
+ * Valid justify-content property values
+ */
 const justifyContentProperties = [
   'flex-start',
   'flex-end',
@@ -30,6 +39,9 @@ const justifyContentProperties = [
   'inherit'
 ];
 
+/**
+ * Valid text-align property values
+ */
 const textAlignProperties = [
   'left',
   'right',
@@ -39,10 +51,25 @@ const textAlignProperties = [
   'inherit'
 ];
 
+/**
+ * Check if align-item property value is valid
+ * @param {string} property
+ * @return {boolean}
+ */
 const isAlignProperty = property => (_.includes(alignItemsProperties, property));
 
+/**
+ * Check if justify-item property value is valid
+ * @param {string} property
+ * @return {boolean}
+ */
 const isJustifyProperty = property => (_.includes(justifyContentProperties, property));
 
+/**
+ * Check if text-align property value is valid
+ * @param {string} property
+ * @return {boolean}
+ */
 const isTextAlignProperty = property => (_.includes(textAlignProperties, property));
 
 export {
